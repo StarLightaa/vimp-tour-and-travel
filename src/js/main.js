becba00c4abd93a6945faee395fcb6a5d8685417
123 <qeqw@eqwdas.com>
2021-03-10 10:42:03 +0300

    header + first screen adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
-$(function() {
+$(function() {})
 
+const menuBtn = document.querySelector('.js--menu-toogle');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function() {
+    this.classList.toggle('menu-toggle--active');
+    menu.classList.toggle('menu--active');
 })
+
